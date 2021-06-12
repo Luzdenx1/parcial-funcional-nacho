e762bc03dd61c9f97bd0fb15609e490a7f16e761
nachoberaza <nachoberaza@gmail.com>
2021-06-12 12:00:18 -0300

    Checkpoint

--- src/Library.hs
+++ src/Library.hs
@@ -108,10 +108,14 @@ modificarSegun :: Id -> [Cuenta] -> (Cuenta -> Cuenta) -> [Cuenta]
 modificarSegun id cuentas trasformacion = 
     map trasformacion (filter (correspondeId id) cuentas) ++ filter (not . correspondeId id) cuentas
 
---- Punto #4 ---
+--- Punto #5 ---
 
+sonEstables :: [Cuenta] -> Bool
+sonEstables = all ((>=0).saldo) 
 
 --- Punto #7 ---
+
+---------------------------------------------------------------- Tests
 
 lista1=[1..20]
 lista2=[1..50]
@@ -121,20 +125,38 @@ tupla=( (+) , (*) )
 
 listaDeListas=[lista1,lista2]
 
-funcionSinPudor :: [[Number]] -> ( Number -> Number , Number -> Number ) -> Number -> Number
+listaPalabras=["hoal","chau"]
+
+
+
+----------------------------------------------------------------------
+
+--funcionSinPudor :: [[Number]] -> ( Number -> Number , String -> String ) -> String -> String
+
+funcionSinPudor :: [[Number]] -> (Number -> Number, b -> b) -> b -> b
 funcionSinPudor x y 
     | (length . filter even . map (fst y) $ head x) > 10 =  id 
     | otherwise = snd y
 
+-- map::(a->a) -> [a] -> [a]
+
 -- Procedimiento de inferencia --
 {-
-    Primero se acomodo la funcion para tener un mejor "panorama". Al observar lo que retorna la funcion podemos notar que x 
-    es una lista ya que se le esta aplicando la funcion head, mientras que y es una tupla ya que se le aplica la funcion fst.
-    Dicho esto, se puede identificar a y como una tupla de funciones ya que luego de que se extrae el primer elemento de la 
+    Primero se acomodo la funcion para tener un mejor "panorama". Al observar lo que retorna la funcion podemos notar que "x" 
+    es una lista ya que se le esta aplicando la funcion head, mientras que "y" es una tupla ya que se le aplica la funcion fst.
+    
+    Dicho esto, se puede identificar a "y" como una tupla de funciones ya que luego de que se extrae el primer elemento de la 
     tupla, se utiliza como "transformacion" en un map.
-    Luego se hace uso de un filter con la condicion de que los elementos sean pares, por lo que se puede deducir que x es una
-    lista de lista de numeros. Ademas, se puede observar que ese filtro se aplica solo a la primer lista de numeros de x, obviamente
+
+    Luego se hace uso de un filter con la condicion de que los elementos sean pares, por lo que se puede deducir que "x" es una
+    lista de listas de numeros.
+    
+    Ademas, se puede observar que ese filtro se aplica solo a la primer lista de numeros de x (por el head), obviamente
     luego de hacerle la transformacion, para luego medir la longitud de la misma. En el caso que la longitud de esa lista sea menor a 10
-    entonces se devuelve la identidad (que seria la misma funcion "funcionSinPudor" )
+    entonces se aplica la identidad y en caso contrario se devuelve el segundo elemento 
+    de "y" que es una funcion. 
+
+    En base a esto ultimo, podemos ver tambien que la "funcionsSinPudor" se esta aplicando parcialmente a un elemento cuyo tipo no se
+    puede determinar 
 
 -}
